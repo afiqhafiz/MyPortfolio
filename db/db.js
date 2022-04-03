11477be871c4f4dba28759fe2d2e5f6a0346ad0a
Muhammad Afiq Redzuan bin Muhammad Hafiz <71485631+afiqhafiz@users.noreply.github.com>
2022-04-03 17:50:40 +0800

    Add files via upload

---
+++ db/db.js
@@ -0,0 +1,199 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Afiq Hafiz. I’m currently a full time cybersecurity student at Universiti Tenaga Nasional.",
+				"I am able to handle multiple tasks on a daily basis and am a dependable person who is great at my management. Other than that, I am always energetic and eager to learn new skills to improve myself to be a beer version. Also, I am actively participating in clubs and organizations at my university.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me feel free to send me an email. My email address is: afiqredz21@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C, C++, Java",
+			color: "1",
+			percentage: "80",
+		},
+
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, CSS",
+			color: "4",
+			percentage: "75",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "7",
+			percentage: "45",
+		},
+		{
+			title: "Tools",
+			skillName: "Chrome DevTools",
+			color: "3",
+			percentage: "35",
+		},
+
+		{
+			title: "Editor",
+			skillName: "Xcode, VScode",
+			color: "6",
+			percentage: "50",
+		},
+	],
+	projects: {
+		web: [
+			{
+				projectName: "RaikanCinta",
+				image: "images/rai.png",
+				summary:
+					"Developed e-invitation letter for wedding",
+				preview: "https://github.com/afiqhafiz/E-Invitation-letter",
+				techStack: [
+					"HTML",
+					"CSS",
+					"JavaScript",
+				],
+			},
+		],
+		software: [
+			
+		],
+		android: [
+			
+		],
+		freelance: [
+			
+		],
+	},
+	experience: [
+		{
+			title: "Students' Representative Council UNITEN 21/22",
+			duration: "November 2021 - Present",
+			subtitle: "President",
+			details: [
+				"Conducting an event related to student and also bring student voices to the management",
+			],
+			tags: [
+				"Leadership",
+			],
+			icon: "qrcode",
+		},
+		{
+			title: "International Business Innovation Competition",
+			duration: "October 2021 - November 2021",
+			subtitle: "Gold Award Winner",
+			details: [
+				"Me and my team present the idea of RuangKerja",
+				"RuangKerja is a non-professional gig work platform",
+			],
+			tags: ["AdobeXD"],
+			icon: "group",
+		},
+
+		{
+			title: "Tealive",
+			duration: "December 2018 - September 2021",
+			subtitle: "Tearista",
+			details: [
+				"I am working as a part-timer at tealive",
+				"learn how to work in a team",
+			],
+			tags: [],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Science (cybersecurity)(Hons.)",
+			duration: "",
+			subtitle: "Universiti Tenaga Nasional",
+			details: [
+				"Dean's list for every semester",
+				"Active participating in clubs.",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System",
+				"Computer Networks",
+				"Compiler Designing",
+			],
+			icon: "graduation-cap",
+		},
+
+		{
+			title: "Foundation in Computer Science",
+			duration: "",
+			subtitle: "Universiti Tenaga Nasional",
+			details: [
+				"Dean's list for every semester",
+				"CGPA 3.93",
+			],
+			tags: [
+				"Operating Systems",
+				"Database Management System",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "SMK Subang Bestari",
+			duration: "",
+			subtitle: "Board of Secondary Education, Rajasthan",
+			details: [
+				"SPM: 5A 3B 1C",
+				"I am taking Computer Science since in secondary school",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics", "Computer Science"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/afiqhafiz",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/afiq-redzuan-3849331a4/",
+				},
+
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Afiq Hafiz",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
